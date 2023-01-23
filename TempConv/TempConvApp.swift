63ccda45e916ab1b877ff0812af0ced106b0483c
AdriGoM <adriangodoymartinez@icloud.com>
2023-01-23 18:28:05 +0000

    Initial Commit

---
+++ TempConv/TempConvApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvApp.swift
+//  TempConv
+//
+//  Created by Adri on 23/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TempConvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
